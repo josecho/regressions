0fcd0c8fbe45f9e4983acf52e82db5c7324c3edc
Jose Villaverde <jlvbalsa@gmail.com>
2020-02-29 20:06:25 +0100

    Variable Decision Boundaries

--- logistic-regression/logistic-regression.js
+++ logistic-regression/logistic-regression.js
@@ -51,11 +51,12 @@ class LogisticRegression {
   predict(observations) {
     return this.processFeatures(observations)
       .matMul(this.weights)
-      .sigmoid();
+      .sigmoid()
+      .round();
   }
 
   test(testFeatures, testLabels) {
-    const predictions = this.predict(testFeatures).round();
+    const predictions = this.predict(testFeatures);
     testLabels = tf.tensor(testLabels);
 
     const incorrect = predictions
