355669381351ae79e9d79aaa6dc8d245be7a4ca6
Jose Villaverde <jlvbalsa@gmail.com>
2020-03-01 12:24:45 +0100

    Classifying Continuous Values

--- multinominal-logistic-regression/index.js
+++ multinominal-logistic-regression/index.js
@@ -8,27 +8,36 @@ let { features, labels, testFeatures, testLabels } = loadCSV(
   "../data/cars.csv",
   {
     dataColumns: ["horsepower", "displacement", "weight"],
-    labelColumns: ["passedemissions"],
+    labelColumns: ["mpg"],
     shuffle: true,
     splitTest: 50,
     converters: {
-      passedemissions: value => {
-        return value === "TRUE" ? 1 : 0;
+      mpg: value => {
+        const mpg = parseFloat(value);
+        if (mpg < 15) {
+          return [1, 0, 0];
+        } else if (mpg < 30) {
+          return [0, 1, 0];
+        } else {
+          return [0, 0, 1];
+        }
       }
     }
   }
 );
 
-const regression = new LogisticRegression(features, labels, {
-  learningRate: 0.5,
-  iterations: 100,
-  batchSize: 10,
-  decisionBoundary: 0.5
-});
+console.log(labels);
 
-regression.train();
-console.log(regression.test(testFeatures, testLabels));
-
-plot({
-  x: regression.costHistory.reverse()
-});
+// const regression = new LogisticRegression(features, labels, {
+//   learningRate: 0.5,
+//   iterations: 100,
+//   batchSize: 10,
+//   decisionBoundary: 0.5
+// });
+//
+// regression.train();
+// console.log(regression.test(testFeatures, testLabels));
+//
+// plot({
+//   x: regression.costHistory.reverse()
+// });
